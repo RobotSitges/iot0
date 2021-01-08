4c19a9ec53fc334955e3601a48c8c45f7b9b5bb5
RobotSitges <44520839+RobotSitges@users.noreply.github.com>
2021-01-08 19:03:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let Temperatura = 0
 basic.showIcon(IconNames.No)
 radio.setGroup(1)
+radio.setTransmitSerialNumber(true)
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     Temperatura = input.temperature()
