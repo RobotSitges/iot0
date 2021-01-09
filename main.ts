11b25310c908f890d6a8bcfd53c01f601bddf582
RobotSitges <44520839+RobotSitges@users.noreply.github.com>
2021-01-09 11:42:16 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let TemperaturaBME280 = 0
 let Temperatura = 0
 basic.showIcon(IconNames.No)
 radio.setGroup(1)
@@ -5,9 +6,13 @@ radio.setTransmitSerialNumber(true)
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     Temperatura = input.temperature()
+    TemperaturaBME280 = Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C)
+    basic.showString("T1:")
     basic.showNumber(Temperatura)
-    radio.sendValue("Conectado", 1)
+    basic.showString("T2:")
+    basic.showNumber(TemperaturaBME280)
     radio.sendValue("Temperatura", Temperatura)
+    radio.sendValue("BME280", TemperaturaBME280)
     basic.showIcon(IconNames.House)
     basic.pause(1000)
 })
